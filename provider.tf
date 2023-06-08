eaa696d3cf0a74396ef0b02d062f9d4de7403652
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-06-08 15:01:58 +0100

    add provider

--- provider.tf
+++ provider.tf
@@ -2,6 +2,12 @@ provider "azurerm" {
   features {}
 }
 
+provider "azurerm" {
+  features {}
+  alias           = "postgres_network"
+  subscription_id = "8cbc6f36-7c56-4963-9d36-739db5d00b27"
+}
+
 provider "random" {}
 
 terraform {
