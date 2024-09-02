4310aded90d86dada6115bbd20b4671302cde063
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2024-09-02 10:42:21 +0100

    testing life cycle

--- main.tf
+++ main.tf
@@ -34,6 +34,10 @@ module "postgresql_flexible" {
   ]
   pgsql_firewall_rules = []
   pgsql_version        = "14"
+
+    lifecycle {
+    prevent_destroy = true
+  }
 }
 
 resource "azurerm_key_vault_secret" "response-db-secret-v14" {
